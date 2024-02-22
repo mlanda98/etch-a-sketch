c34d280704ad64a92179c634c8c33d82d3c2de80
Mayra Landa <mlandae16@gmail.com>
2024-02-22 13:38:45 -0800

    Add hovering
    When hovering over the grid it leaves a drawing by changing the background color

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ let grid = document.getElementById("grid");
 for (let i = 0; i < size * size; i++){
   const div = document.createElement("div");
   div.classList.add("grid-item");
-  div.textContent = i;
-  grid.appendChild(div);
   
+  div.addEventListener("mouseover", addDrawing);
+  grid.appendChild(div);
 
 };
-
-
 };
 
 makeGrid(16);
+
+function addDrawing(e){
+  e.target.style = "background: blue"
+  e.target.removeEventListener("mouseover", addDrawing);
+}
